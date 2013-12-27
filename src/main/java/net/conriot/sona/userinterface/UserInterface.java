1cb526e876698daa756a7cbb52ea9187f93e91a1
Endain <endain.gaming@gmail.com>
2013-12-26 17:12:17 -0800

    Filled out initial projects skeleton

---
+++ src/main/java/net/conriot/sona/userinterface/UserInterface.java
@@ -0,0 +1,15 @@
+package net.conriot.sona.userinterface;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UserInterface extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		// TODO
+	}
+	
+	@Override
+	public void onDisable() {
+		// Nothing to do here
+	}
+}
